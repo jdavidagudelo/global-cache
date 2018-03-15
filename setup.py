d5c4ebc741858d7f5f9ea81b2c47b8fd22be091d
jdavidagudelo <jdaaa2009@gmail.com>
2018-03-15 07:15:49 -0500

    Fixing some issues.

--- setup.py
+++ setup.py
@@ -7,5 +7,5 @@ setup(name='ubidots_global_cache',
       author='jdavidagudelo',
       author_email='jdaaa2009@gmail.com',
       license='MIT',
-      packages=['ubidots_global_cache'],
+      packages=['global_cache'],
       zip_safe=False, install_requires=['redis'])
